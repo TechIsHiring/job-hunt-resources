f9e6c028ca69d3a89576f04e5c5e3f218c899731
Chad Stewart <chadrhonanstewart@gmail.com>
2024-07-17 11:30:02 -0500

    Added accordion where the resources will go and messed around with layout a bit.

--- src/components/atoms/card/main-layout-card.tsx
+++ src/components/atoms/card/main-layout-card.tsx
@@ -5,7 +5,7 @@ interface CardProps {
 export const MainLayoutCard: React.FC<CardProps> = ({ children }) => {
   return (
     <main
-      className={`flex flex-col max-w-[1024px] border-x-[1px] shadow-2xl shadow-white p-5 h-full`}
+      className={`flex flex-col gap-4 max-w-[1024px] w-full border-x-[1px] shadow-2xl shadow-white p-5 h-full`}
     >
       {children}
     </main>
--- src/components/atoms/logo/index.tsx
+++ src/components/atoms/logo/index.tsx
@@ -2,7 +2,7 @@ import Link from "@/components/atoms/link";
 
 export const Logo = () => {
   return (
-    <h1 className="flex gap-2 text-2xl font-logo">
+    <h1 className="flex sm:flex-row flex-col gap-2 text-2xl font-logo">
       <Link href="https://www.techishiring.com">
         <span className="text-logo">TechIsHiring</span>
       </Link>
