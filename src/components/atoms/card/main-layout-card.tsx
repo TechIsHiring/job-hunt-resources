c62360e195163e257feec3804bf74a2d9fe3a253
jean-philippe <jp@zelexio.com>
2024-07-18 12:27:05 -0400

    backgrouund upgrade

--- src/components/atoms/card/main-layout-card.tsx
+++ src/components/atoms/card/main-layout-card.tsx
@@ -5,7 +5,7 @@ interface CardProps {
 export const MainLayoutCard: React.FC<CardProps> = ({ children }) => {
   return (
     <div
-      className={`flex flex-col gap-4 max-w-[1024px] w-full border-x-[1px] shadow-2xl shadow-white p-5 h-full`}
+      className={`flex flex-col gap-4 max-w-[1024px] w-full border-x-md shadow-lg shadow-white p-5 h-full`}
     >
       {children}
     </div>
