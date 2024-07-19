5fc8d995f2fc1c7196d6a8087507354515b6740c
Chad Stewart <chadrhonanstewart@gmail.com>
2024-07-19 10:30:18 -0500

    Added modal to project.

--- src/components/organisms/footer/index.tsx
+++ src/components/organisms/footer/index.tsx
@@ -1,13 +1,59 @@
 import { Button } from "@/components/atoms/button";
 import { Divider } from "@/components/atoms/divider";
 import { FooterInfo } from "@/components/molecules/footer-info";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
 
 export const Footer = () => {
   return (
     <footer className="flex flex-col gap-3 p-5 sticky bottom-0">
       <Divider />
       <div className="flex sm:flex-row gap-8 flex-col pt-3 items-center justify-between">
-        <Button className="w-fit">+ Add a Resource</Button>
+        <Dialog>
+          <DialogTrigger asChild>
+            <Button className="w-fit">+ Add a Resource</Button>
+          </DialogTrigger>
+          <DialogContent className="bg-black sm:max-w-[425px]">
+            <DialogHeader>
+              <DialogTitle>Edit profile</DialogTitle>
+              <DialogDescription>
+                Make changes to your profile here. Click save when youre done.
+              </DialogDescription>
+            </DialogHeader>
+            <div className="grid gap-4 py-4">
+              <div className="grid grid-cols-4 items-center gap-4">
+                <label htmlFor="name" className="text-right">
+                  Name
+                </label>
+                <input
+                  id="name"
+                  defaultValue="Pedro Duarte"
+                  className="col-span-3"
+                />
+              </div>
+              <div className="grid grid-cols-4 items-center gap-4">
+                <label htmlFor="username" className="text-right">
+                  Username
+                </label>
+                <input
+                  id="username"
+                  defaultValue="@peduarte"
+                  className="col-span-3"
+                />
+              </div>
+            </div>
+            <DialogFooter>
+              <input type="submit" />
+            </DialogFooter>
+          </DialogContent>
+        </Dialog>
         <FooterInfo />
       </div>
     </footer>
