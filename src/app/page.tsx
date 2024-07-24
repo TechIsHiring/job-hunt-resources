46eec71f2eff51b375579247899cdb4050f80ea9
Chad Stewart <chadrhonanstewart@gmail.com>
2024-07-24 10:39:21 -0500

    Set revalidation of requests for app to be an hour long.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,12 @@
 import { JobHuntResourceList } from "@/components/organisms/job-hunt-resource-list";
 import { getResourceData } from "@/lib/github-api/get-resource-data";
 
+/* 
+  Sets revalidation for all requests in Next.js' App Router for this route segment to be an hour:
+  https://nextjs.org/docs/app/api-reference/file-conventions/route-segment-config#revalidate
+*/
+export const revalidate = 3600;
+
 export default async function Home() {
   const resourceData = await getResourceData();
 
