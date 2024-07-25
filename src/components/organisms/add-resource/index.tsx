b877050ac57c435baaefcf99dbd99c51ab092bc0
Chad Stewart <chadrhonanstewart@gmail.com>
2024-07-25 15:28:53 -0500

    Fixed issue where modal wasn't scrollable if the modal height was too big.

--- src/components/organisms/add-resource/index.tsx
+++ src/components/organisms/add-resource/index.tsx
@@ -15,7 +15,7 @@ export const AddResource = () => {
       <DialogTrigger asChild>
         <Button className="w-fit">+ Add a Resource</Button>
       </DialogTrigger>
-      <DialogContent className="bg-black sm:max-w-[600px]">
+      <DialogContent className="bg-black sm:max-w-[600px] overflow-y-scroll max-h-screen">
         <DialogHeader className="flex flex-col gap-4 ">
           <DialogTitle>Add a Tech Job Search Resource</DialogTitle>
           <DialogDescription>
