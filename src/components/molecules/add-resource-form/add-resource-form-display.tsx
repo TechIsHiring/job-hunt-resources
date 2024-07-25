846a947ee5a3f7d7e88d16dc0f93bc583a8c4c8f
Chad Stewart <chadrhonanstewart@gmail.com>
2024-07-25 12:47:49 -0500

    Made form from shadcn components.

--- src/components/molecules/add-resource-form/add-resource-form-display.tsx
+++ src/components/molecules/add-resource-form/add-resource-form-display.tsx
@@ -1,81 +1,183 @@
 "use client";
 
 import { DialogFooter } from "@/components/ui/dialog";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Textarea } from "@/components/ui/textarea";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
 
 interface AddResourceFormProps {
   categories: string[];
 }
 
+const FormSchema = z.object({
+  name: z.any(),
+  url: z.any(),
+  description: z.any(),
+  owner: z.any(),
+  submitted_by: z.any(),
+  category: z.any(),
+});
+
 export const AddResourceFormDisplay = ({
   categories,
 }: AddResourceFormProps) => {
+  const form = useForm<z.infer<typeof FormSchema>>({
+    resolver: zodResolver(FormSchema),
+    defaultValues: {
+      name: "",
+      url: "",
+      description: "",
+      owner: "",
+      submitted_by: "",
+      category: "",
+    },
+  });
+
+  function onSubmit(data: z.infer<typeof FormSchema>) {
+    console.log(data);
+  }
+
   return (
-    <>
-      <div className="grid gap-4 py-4">
-        <div className="grid grid-cols-4 items-center gap-4">
-          <label htmlFor="name" className="text-right">
-            Name
-          </label>
-          <input
-            id="name"
-            placeholder="Name"
-            className="text-black col-span-3"
-          />
-        </div>
-        <div className="grid grid-cols-4 items-center gap-4">
-          <label htmlFor="url" className="text-right">
-            Url
-          </label>
-          <input id="url" placeholder="Url" className="text-black col-span-3" />
-        </div>
-        <div className="grid grid-cols-4 items-center gap-4">
-          <label htmlFor="description" className="text-right">
-            Description
-          </label>
-          <textarea
-            id="description"
-            className="text-black col-span-3 resize-none"
-          />
-        </div>
-        <div className="grid grid-cols-4 items-center gap-4">
-          <label htmlFor="owner" className="text-right">
-            Owner
-          </label>
-          <input
-            id="owner"
-            placeholder="Owner"
-            className="text-black col-span-3"
-          />
-        </div>
-        <div className="grid grid-cols-4 items-center gap-4">
-          <label htmlFor="submitted_by" className="text-right">
-            Submitter Name
-          </label>
-          <input
-            id="submitted_by"
-            placeholder="Submitter Name"
-            className="text-black col-span-3"
-          />
-        </div>
-        <div className="grid grid-cols-4 items-center gap-4">
-          <label htmlFor="category" className="text-right">
-            Category
-          </label>
-          <select
-            id="category"
-            className="text-black col-span-3 capitalize py-[3px]"
-          >
-            {categories?.map((category, key) => (
-              <option key={key} value={category}>
-                {category}
-              </option>
-            ))}
-          </select>
-        </div>
-      </div>
-      <DialogFooter>
-        <input type="submit" />
-      </DialogFooter>
-    </>
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
+        <FormField
+          control={form.control}
+          name="name"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Name</FormLabel>
+              <FormControl>
+                <Input className="text-black" placeholder="Name" {...field} />
+              </FormControl>
+              <FormDescription>
+                The name of the job search resource
+              </FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="url"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Url</FormLabel>
+              <FormControl>
+                <Input className="text-black" placeholder="Url" {...field} />
+              </FormControl>
+              <FormDescription>
+                A working url to the job search resource
+              </FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="description"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Description</FormLabel>
+              <FormControl>
+                <Textarea
+                  placeholder="Description"
+                  className="text-black resize-none"
+                  {...field}
+                />
+              </FormControl>
+              <FormDescription>
+                A description of the job search resource
+              </FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="owner"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Owner</FormLabel>
+              <FormControl>
+                <Input className="text-black" placeholder="Owner" {...field} />
+              </FormControl>
+              <FormDescription>The owner of the resource</FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="category"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Category</FormLabel>
+              <FormControl>
+                <Select
+                  onValueChange={field.onChange}
+                  defaultValue={field.value}
+                >
+                  <SelectTrigger className="text-black capitalize w-[180px]">
+                    <SelectValue placeholder="Select a Category" />
+                  </SelectTrigger>
+                  <SelectContent>
+                    {categories?.map((category, key) => (
+                      <SelectItem
+                        key={key}
+                        value={category}
+                        className="text-black capitalize"
+                      >
+                        {category}
+                      </SelectItem>
+                    ))}
+                  </SelectContent>
+                </Select>
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="submitted_by"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Submitter Name</FormLabel>
+              <FormControl>
+                <Input
+                  className="text-black"
+                  placeholder="Your Name"
+                  {...field}
+                />
+              </FormControl>
+              <FormDescription>Your name</FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <DialogFooter>
+          <Button type="submit">Submit</Button>
+        </DialogFooter>
+      </form>
+    </Form>
   );
 };
