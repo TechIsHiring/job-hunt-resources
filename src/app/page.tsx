570e6110c1a51ba6ccfb8f215fff856faa345f80
Chad Stewart <chadrhonanstewart@gmail.com>
2024-07-28 10:06:53 -0500

    Set app to dynamically render at request time as opposed to render statically on build time.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,23 @@ import { JobHuntResourceList } from "@/components/organisms/job-hunt-resource-li
 import { getObjectKeys } from "@/lib/get-object-keys";
 import { getResourceData } from "@/lib/github-api/get-resource-data/";
 
+// Route Segment Config
+
+/*
+  dynamic: Sets when pages are rendered in the app
+  https://nextjs.org/docs/app/api-reference/file-conventions/route-segment-config#dynamic
+*/
+
+export const dynamic = "force-dynamic";
+
 /* 
-  Sets revalidation for all requests in Next.js' App Router for this route segment to be an hour:
+  revalidate: Sets the caching behaviour of fetch requests
   https://nextjs.org/docs/app/api-reference/file-conventions/route-segment-config#revalidate
 */
 
 export const revalidate = 3600;
+
+// END Route Segment Config
 
 export default async function Home() {
   const { resourceData } = await getResourceData();
