b0c7148489afb85a1535c4e6c0d4d326a2c5261c
Chad Stewart <chadrhonanstewart@gmail.com>
2024-07-29 10:52:50 -0500

    Added icons for category titles.

---
+++ src/components/molecules/category-icon/index.tsx
@@ -0,0 +1,28 @@
+import { Icon } from "@/components/atoms/icon";
+import { IconType } from "react-icons";
+import { FaPeopleCarryBox } from "react-icons/fa6";
+import { PiBooksLight } from "react-icons/pi";
+import { FiGithub } from "react-icons/fi";
+import { IoPersonSharp } from "react-icons/io5";
+
+interface CategoryIconProps {
+  categoryTitle: string;
+}
+
+export const CategoryIcon = ({ categoryTitle }: CategoryIconProps) => {
+  const categoryIcons: { [key: string]: IconType } = {
+    books: PiBooksLight,
+    communities: FaPeopleCarryBox,
+    repos: FiGithub,
+    people: IoPersonSharp,
+  };
+
+  return (
+    <Icon
+      Icon={categoryIcons[categoryTitle]}
+      iconAlt={`Icon for ${categoryTitle} category`}
+      size={24}
+      color="white"
+    />
+  );
+};
--- src/components/organisms/job-hunt-resource-list/index.tsx
+++ src/components/organisms/job-hunt-resource-list/index.tsx
@@ -9,6 +9,7 @@ import { Card } from "@/components/atoms/card";
 import { ResourceData } from "@/lib/types/job-resource-types";
 import { ResourceCardBody } from "@/components/molecules/resources-card-body";
 import { Divider } from "@/components/atoms/divider";
+import { CategoryIcon } from "@/components/molecules/category-icon";
 
 interface JobHuntResourceListProps {
   jobResources: ResourceData;
@@ -24,7 +25,10 @@ export const JobHuntResourceList = ({
       {resourcesObjKey.map((jobResourceIndex, key) => (
         <section key={key} className="flex flex-col gap-8">
           <Divider className="border-slate-300 border-t-[1px]" />
-          <h2 className="capitalize font-semibold pl-5">{jobResourceIndex}</h2>
+          <span className="flex gap-4 px-2">
+            <CategoryIcon categoryTitle={jobResourceIndex} />
+            <h2 className="capitalize font-semibold">{jobResourceIndex}</h2>
+          </span>
           <Accordion
             type="single"
             collapsible
