0db296e3684fae874284f5e120b6d1913bab126e
Chad Stewart <chadrhonanstewart@gmail.com>
2024-07-31 08:20:54 -0500

    Added copy in the header to give a quick description of what the app is.

--- src/components/organisms/header/index.tsx
+++ src/components/organisms/header/index.tsx
@@ -3,9 +3,14 @@ import { ThemeToggle } from "@/components/atoms/theme-toggle";
 
 export const Header = () => {
   return (
-    <header className="flex justify-between">
-      <Logo />
-      <ThemeToggle />
+    <header className="flex flex-col gap-4">
+      <div className="flex justify-between">
+        <Logo />
+        <ThemeToggle />
+      </div>
+      <span>
+        A community-driven list of job search resources for Tech Professionals.
+      </span>
     </header>
   );
 };
