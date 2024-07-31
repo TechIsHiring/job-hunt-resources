9695934b4e50b7b155972a07e7e18568407a51a8
Chad Stewart <chadrhonanstewart@gmail.com>
2024-07-31 08:27:22 -0500

    Changed copy of add resource form to reflect that resources don't immediately show up after submitting them.

--- src/components/organisms/add-resource/index.tsx
+++ src/components/organisms/add-resource/index.tsx
@@ -13,14 +13,15 @@ export const AddResource = () => {
   return (
     <Dialog>
       <DialogTrigger asChild>
-        <Button className="w-fit text-white">+ Add a Resource</Button>
+        <Button className="w-fit text-white">+ Submit a Resource</Button>
       </DialogTrigger>
       <DialogContent className="bg-bglightmode text-fglightmode dark:bg-bgdarkmode dark:text-fgdarkmode sm:max-w-[600px] border-0 sm:border overflow-y-auto max-h-screen">
         <DialogHeader className="flex flex-col gap-4 ">
           <DialogTitle>Add a Tech Job Search Resource</DialogTitle>
           <DialogDescription className="font-medium">
             If you have a resource that will be helpful to other tech
-            professionals on the job hunt, use this form to add it to the list!
+            professionals on the job search, submit it here for review to be
+            added to the list!
           </DialogDescription>
         </DialogHeader>
         <AddResourceForm />
