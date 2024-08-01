b55358144b9321ee7e335d598009801f08b9a014
Chad Stewart <chadrhonanstewart@gmail.com>
2024-08-01 14:26:38 -0500

    Fixing issue with text alignment in mobile screens.

--- src/components/molecules/resource-card-title/index.tsx
+++ src/components/molecules/resource-card-title/index.tsx
@@ -8,9 +8,9 @@ export const ResourceCardTitle = ({
   outline,
 }: ResourceCardTitleProps) => {
   return (
-    <div className="flex flex-col items-start w-full gap-6">
-      <span className="capitalize font-semibold">{name}</span>
-      <span className="italic text-sm">{outline}</span>
+    <div className="flex flex-col sm:items-start w-full gap-6">
+      <span className="capitalize font-semibold sm:text-left">{name}</span>
+      <p className="italic text-sm sm:text-left">{outline}</p>
     </div>
   );
 };
