77e50fbd04a83f0183986ffebaaa724e11f05912
Chad Stewart <chadrhonanstewart@gmail.com>
2024-08-07 09:26:49 -0500

    Installed PostHog into app.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,8 +4,15 @@ import { HomePageLayout } from "@/components/templates/homepagelayout";
 import { MainLayoutCard } from "@/components/atoms/card";
 import { Header } from "@/components/organisms/header";
 import { Footer } from "@/components/organisms/footer";
-import { Analytics } from "@vercel/analytics/react";
 import { ThemeProvider } from "@/components/particles/theme-provider";
+import dynamic from "next/dynamic";
+
+const AppAnalytics = dynamic(
+  () => import("../components/particles/analytics"),
+  {
+    ssr: false,
+  }
+);
 
 export const metadata: Metadata = {
   title: "TechIsHiring Job Search Resources",
@@ -21,18 +28,19 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
-        <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-          <HomePageLayout>
-            <MainLayoutCard>
-              <div className="flex flex-col gap-4 pt-5">
-                <Header />
-                {children}
-              </div>
-              <Footer />
-            </MainLayoutCard>
-          </HomePageLayout>
-        </ThemeProvider>
-        <Analytics />
+        <AppAnalytics>
+          <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+            <HomePageLayout>
+              <MainLayoutCard>
+                <div className="flex flex-col gap-4 pt-5">
+                  <Header />
+                  {children}
+                </div>
+                <Footer />
+              </MainLayoutCard>
+            </HomePageLayout>
+          </ThemeProvider>
+        </AppAnalytics>
       </body>
     </html>
   );
