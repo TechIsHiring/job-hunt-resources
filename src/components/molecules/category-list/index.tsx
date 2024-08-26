d572e47624e3ff62b91c68cc92b46be4dc43afbb
Chad Stewart <chadrhonanstewart@gmail.com>
2024-08-26 08:56:21 -0500

    Forgot to add replace underscores with spaces in the categorylist title.

--- src/components/molecules/category-list/index.tsx
+++ src/components/molecules/category-list/index.tsx
@@ -12,7 +12,7 @@ export const CategoryList = ({ categoryList }: CategoryListProps) => {
         <a key={key} href={`#${category}`}>
           <Button className="flex gap-2 capitalize rounded-full text-base font-semibold hover:bg-slate-400 bg-slate-200 text-black dark:hover:bg-slate-800 dark:bg-slate-600 dark:text-white">
             <CategoryIcon categoryTitle={category} />
-            {category}
+            {category.replace("_", " ")}
           </Button>
         </a>
       ))}
