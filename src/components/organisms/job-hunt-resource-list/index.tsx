72d918e66130c2f0210a159750179e2a7f863d02
Chad Stewart <chadrhonanstewart@gmail.com>
2024-08-26 08:25:45 -0500

    Added nav buttons to available categories.

--- src/components/organisms/job-hunt-resource-list/index.tsx
+++ src/components/organisms/job-hunt-resource-list/index.tsx
@@ -10,6 +10,7 @@ import { ResourceData } from "@/lib/types/job-resource-types";
 import { ResourceCardBody } from "@/components/molecules/resources-card-body";
 import { Divider } from "@/components/atoms/divider";
 import { CategoryTitle } from "@/components/molecules/category-title";
+import { CategoryList } from "@/components/molecules/category-list";
 
 interface JobHuntResourceListProps {
   jobResources: ResourceData;
@@ -22,9 +23,13 @@ export const JobHuntResourceList = ({
 }: JobHuntResourceListProps) => {
   return (
     <Accordion type="single" collapsible className="flex flex-col gap-6 w-full">
+      <Divider className="border-fglightmode/60 dark:border-slate-300 border-t-[1px]" />
+      <CategoryList categoryList={resourcesObjKey} />
       {resourcesObjKey.map((jobResourceIndex, key) => (
         <section key={key} className="flex flex-col gap-8">
-          <Divider className="border-fglightmode/60 dark:border-slate-300 border-t-[1px]" />
+          {key !== 0 && (
+            <Divider className="border-fglightmode/60 dark:border-slate-300 border-t-[1px]" />
+          )}
           <CategoryTitle categoryName={jobResourceIndex} />
           <div className="flex flex-col gap-6 w-full">
             {jobResources[jobResourceIndex].map((jobResource, key) => (
